ffc7d2bd0752f32372d0202d729cabb950ef4b54
zuliandri2 <zuliyandri@gmail.com>
2023-11-04 17:44:20 +0700

    update role

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,10 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\IsAdmin;
+use App\Http\Middleware\IsManager;
+use App\Http\Middleware\IsUser;
+use App\Http\Middleware\ProductManager;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -64,5 +68,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'productmanager' => ProductManager::class,
+        'isUser' => IsUser::class,
     ];
 }
---
+++ app/Http/Middleware/ProductManager.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductManager
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && (Auth::user()->isAdmin() || Auth::user()->isManager())) {
+            return $next($request);
+        }
+
+        return redirect('/');
+    }
+}
